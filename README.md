1d167e7b5bdc90767cd46452d1c7c266f5f83a3b
kongtaemin <rhdxoals@gmail.com>
2022-01-26 02:49:42 +0900

    vanilla js redux example done 🎉

--- README.md
+++ README.md
@@ -38,3 +38,80 @@ minusBtn.addEventListener("click", () => {
   countStore.dispatch({ type: MINUS });
 });
 ```
+
+## TODO app
+
+### index.html
+
+```html
+<h1>Vanilla JS Redux TODO</h1>
+<form class="d-flex align-items-center mx-auto justify-content-center">
+  <input type="text" class="d-flex mb-0" placeholder="new todo" />
+  <button class="py-1">ADD</button>
+</form>
+<ul></ul>
+```
+
+### index.js
+
+```js
+import { createStore } from "redux";
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+const reducer = (state = [], action) => {
+  // console.log(action);
+  switch (action.type) {
+    case "ADD":
+      return [...state, createNewTodo(action.payload)];
+    case "REMOVE":
+      return removeTodo(state, action.payload);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  updateUI(store.getState());
+  console.log(store.getState());
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  store.dispatch({ type: "ADD", payload: input.value });
+  input.value = "";
+});
+
+function createNewTodo(text) {
+  return {
+    id: Date.now(),
+    text: text,
+  };
+}
+
+function removeTodo(todos, targetId) {
+  const newTodo = todos.filter((todo) => todo.id !== targetId);
+  return newTodo;
+}
+
+function updateUI(todos) {
+  ul.innerHTML = "";
+  todos.forEach((todo) => {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    button.addEventListener("click", () => {
+      store.dispatch({ type: "REMOVE", payload: todo.id });
+    });
+    button.setAttribute("class", "py-1");
+    button.textContent = "삭제";
+    li.setAttribute("class", "list-unstyled");
+    li.textContent = todo.text;
+    li.appendChild(button);
+    ul.appendChild(li);
+  });
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,34 +1,59 @@
 import { createStore } from "redux";
 
-const addBtn = document.querySelector(".add");
-const minusBtn = document.querySelector(".minus");
-const count = document.querySelector(".count");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const ADD = "increase";
-const MINUS = "decrease";
-
-const reducer = (state = 0, action) => {
-  console.log(state, action);
-
+const reducer = (state = [], action) => {
+  // console.log(action);
   switch (action.type) {
-    case ADD:
-      return state + 1;
-    case MINUS:
-      return state - 1;
+    case "ADD":
+      return [...state, createNewTodo(action.payload)];
+    case "REMOVE":
+      return removeTodo(state, action.payload);
     default:
       return state;
   }
 };
-const countStore = createStore(reducer);
 
-countStore.subscribe(() => {
-  console.log("countStore is changed!", countStore.getState());
-  count.innerHTML = countStore.getState();
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  updateUI(store.getState());
+  console.log(store.getState());
 });
 
-addBtn.addEventListener("click", () => {
-  countStore.dispatch({ type: ADD });
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  store.dispatch({ type: "ADD", payload: input.value });
+  input.value = "";
 });
-minusBtn.addEventListener("click", () => {
-  countStore.dispatch({ type: MINUS });
-});
+
+function createNewTodo(text) {
+  return {
+    id: Date.now(),
+    text: text,
+  };
+}
+
+function removeTodo(todos, targetId) {
+  const newTodo = todos.filter((todo) => todo.id !== targetId);
+  return newTodo;
+}
+
+function updateUI(todos) {
+  ul.innerHTML = "";
+  todos.forEach((todo) => {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    button.addEventListener("click", () => {
+      store.dispatch({ type: "REMOVE", payload: todo.id });
+    });
+    button.setAttribute("class", "py-1");
+    button.textContent = "삭제";
+    li.setAttribute("class", "list-unstyled");
+    li.textContent = todo.text;
+    li.appendChild(button);
+    ul.appendChild(li);
+  });
+}
